0e8e3e8a02bfcc94a1313643f159d33e44da3e99
Bill Odell <billodell5@gmail.com>
2019-12-27 18:33:47 -0800

    refactored to one page

--- app/main.js
+++ app/main.js
@@ -1,6 +1,3 @@
-import { Player, Computer } from "./paddle";
-import { Ball } from "./ball";
-
 var animate = window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
@@ -11,7 +8,7 @@ var width = 400;
 var height = 600;
 canvas.width = width;
 canvas.height = height;
-export var context = canvas.getContext('2d');
+var context = canvas.getContext('2d');
 
 var player = new Player();
 var computer = new Computer();
@@ -19,11 +16,10 @@ var ball = new Ball(200, 300);
 
 
 var update = function() {
-
+    ball.update(player.paddle, computer.paddle);
 }
 
 var render = function() {
-    console.log('main render')
     context.fillStyle = "#FF00FF";
     context.fillRect(0, 0, width, height);
     player.render();
@@ -40,4 +36,88 @@ var step = function() {
 window.onload = function() {
     document.body.appendChild(canvas);
     animate(step)
+}
+
+
+function Paddle(x,y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.x_speed = 0;
+    this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+    context.fillStyle = "#0000FF";
+    context.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function Player() {
+    this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+Player.prototype.render = function() {
+    this.paddle.render();
+}
+
+function Computer() {
+    this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+Computer.prototype.render = function() {
+    this.paddle.render();
+}
+
+function Ball(x, y) {
+    this.x = x;
+    this.y = y;
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.radius = 5;
+}
+
+Ball.prototype.render = function() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+    context.fillStyle = "#000000";
+    context.fill();
+}
+
+Ball.prototype.update = function(paddle1, paddle2) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x + 5;
+    var bottom_y = this.y + 5
+
+    if (this.x - 5 < 0) {
+        this.x = 5;
+        this.x_speed = -this.x_speed;
+    } else if (this.x + 5 > 400) {
+        this.x = 395;
+        this.x_speed = -this.x_speed;
+    }
+
+    if (this.y < 0 || this.y > 600) {
+        this.x_speed = 0;
+        this.y_speed = 3;
+        this.x = 200;
+        this.y = 300;
+    }
+
+    if (top_y > 300) {
+        if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+            this.y_speed = -3;
+            this.x_speed += paddle1.x_speed / 2;
+            this.y += this.y_speed;
+        }
+    } else {
+        if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+          this.y_speed = 3;
+          this.x_speed += (paddle2.x_speed / 2);
+          this.y += this.y_speed;
+        }
+    }
 }
